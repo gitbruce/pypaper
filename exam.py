4fb3c0bbc9fd3801582debf97c92b61982a13f98
Bruce <mantianly@163.com>
2018-12-11 00:54:55 +0800

    removed

--- exam.py
+++ exam.py
@@ -82,51 +82,11 @@ def test2():
 	cv2.destroyAllWindows()
 
 
-import os
-import cv2
-import math
-import random
-import numpy as np
-from scipy import misc, ndimage
-
-
-def rotate():
-	filepath = './exam/'
-
-	for filename in os.listdir(filepath):
-		if all([not filename.endswith('.png'), not filename.endswith('.jpg')]):
-			continue
-		img = cv2.imread(filepath + filename)
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-
-		# 霍夫变换
-		lines = cv2.HoughLines(edges, 1, np.pi / 180, 0)
-		for rho, theta in lines[0]:
-			a = np.cos(theta)
-			b = np.sin(theta)
-			x0 = a * rho
-			y0 = b * rho
-			x1 = int(x0 + 1000 * (-b))
-			y1 = int(y0 + 1000 * (a))
-			x2 = int(x0 - 1000 * (-b))
-			y2 = int(y0 - 1000 * (a))
-		if x1 == x2 or y1 == y2:
-			continue
-		t = float(y2 - y1) / (x2 - x1)
-		rotate_angle = math.degrees(math.atan(t))
-		if rotate_angle > 45:
-			rotate_angle = -90 + rotate_angle
-		elif rotate_angle < -45:
-			rotate_angle = 90 + rotate_angle
-		rotate_img = ndimage.rotate(img, rotate_angle)
-		cv2.imwrite(filepath + 'processed/' + filename, rotate_img)
-
 def main():
 	# teacher_removal(filepath)
-	# test('./newImages/rectangle.png')
+	test('./newImages/rectangle.png')
 	# test2()
-	rotate()
+	# rotate()
 
 
 if __name__ == "__main__":
